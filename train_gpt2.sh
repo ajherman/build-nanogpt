f033ce094fdeb81d90ca6d3a844bdcce256fea4a
Ari Herman <ari.j.herman@gmail.com>
2024-08-30 17:52:29 -0700

    wip

--- train_gpt2.sh
+++ train_gpt2.sh
@@ -3,7 +3,7 @@
 #SBATCH --time 2:00:00
 #SBATCH -N 1           
 #SBATCH -p shared-redstone
-#SBATCH -C gpu_count:4
+#SBATCH --gres=gpu:4
 #SBATCH --mem=0
 #SBATCH --exclusive
 #SBATCH --cpus-per-task=16
