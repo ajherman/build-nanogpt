9bdcc3b5be3b74d9e2e5ae7f04f99eccbaddb1c2
Ari Herman <ari.j.herman@gmail.com>
2024-08-31 01:18:06 -0600

    wip

--- train_gpt2.sh
+++ train_gpt2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH --job-name=main
-#SBATCH --time 0:30:00
+#SBATCH --time 2:00:00
 #SBATCH -N 1           
 #SBATCH -p shared-redstone
 #SBATCH -C gpu_count:4
 #SBATCH --mem=0
 #SBATCH --exclusive
 #SBATCH --cpus-per-task=16
-#SBATCH --array=1-5%1  # 100 jobs in the array, 1 running at a time
+#SBATCH --array=1-100%1  # 100 jobs in the array, 1 running at a time
 
 module load miniconda3
 
@@ -39,15 +39,15 @@ source activate /vast/home/ajherman/miniconda3/envs/transformer
 
 
 
-# srun -o renormalize.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --mlp_no_bias --mlp_renormalize --output_dir=renormalize" 
+srun -o renormalize.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --mlp_no_bias --mlp_renormalize --output_dir=renormalize" 
 
-# srun -o original.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --output_dir=original" 
+srun -o original.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --output_dir=original" 
 
-# srun -o nobias.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --mlp_no_bias --output_dir=nobias" 
+srun -o nobias.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --mlp_no_bias --output_dir=nobias" 
 
-# srun -o rmsnorm.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --mlp_norm_type=rms --output_dir=rmsnorm" 
+srun -o rmsnorm.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --mlp_norm_type=rms --output_dir=rmsnorm" 
 
-# srun -o renormalize_noskip.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --mlp_no_skip --mlp_renormalize --output_dir=renormalize_noskip" 
+srun -o renormalize_noskip.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --mlp_no_skip --mlp_renormalize --output_dir=renormalize_noskip" 
 
 srun -o noskip.out --ntasks=1 -N 1 bash -c "torchrun --nproc_per_node=4 train_gpt2.py --micro_batch_size=16 --mlp_no_skip --output_dir=noskip" 
 
